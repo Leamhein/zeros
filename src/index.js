115c4c3545728a564ef50a3e5fe7c04783b19165
Vladimir <Leamhein@hotmail.com>
2018-02-22 17:22:30 +0300

    Done 100/100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  var n = []; //geometric progression of the number 5
+  var zeros = 0; //number of our zeros
+  for (let i = 1; i > 0; i++) { /*fill our array with numbers obtained
+                                with geometric progression of the number 5.
+                                this numbers must be less then incoming number*/
+    if (n[i-2] > number) {
+      n.splice(i-2, 1);
+      break;
+    };
+    n[i-1] = Math.pow(5, i);
+  };
+  for (let i = 0; i < n.length; i++) {
+    zeros += Math.floor (number / n[i]); //count our zeros
+  };
+  return zeros;
+};
